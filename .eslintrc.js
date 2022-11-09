2ec0254c5585a7465fb17a1814b175c03e68e855
LuanaMayumi <luh.mayumi@gmail.com>
2022-11-09 15:39:03 -0300

    criando validador de campos - ex Front - dia 01

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'trybe-frontend',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
